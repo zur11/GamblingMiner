19b30dd46ec784c0a875c4db2466ebdaabca57ac
zur11 <cibka11@gmail.com>
2026-02-13 10:04:47 -0500

    changed DiceResult class to sealed and mandatory constructor

--- Scripts/Dice/DiceResult.cs
+++ Scripts/Dice/DiceResult.cs
@@ -1,19 +1,43 @@
 namespace Scripts.Dice
 {
-	public class DiceResult
+	public sealed class DiceResult
 	{
-		public int Roll { get; init; }
-		public bool IsWin { get; init; }
+		public int Roll { get; }
+		public bool IsWin { get; }
 
-		public int WinMin { get; init; }
-		public int WinMax { get; init; }
+		public decimal Bet { get; }
+		public decimal Chance { get; }
+		public decimal Multiplier { get; }
+		public bool IsHigh { get; }
 
-		public decimal Bet { get; init; }
-		public decimal Profit { get; init; }
-		public decimal BalanceAfter { get; init; }
+		public decimal Profit { get; }
+		public decimal BalanceAfter { get; }
 
-		public decimal Chance { get; init; }
-		public decimal Multiplier { get; init; }
-		public bool IsHigh { get; init; }
+		public int WinMin { get; }
+		public int WinMax { get; }
+
+		public DiceResult(
+			int roll,
+			bool isWin,
+			decimal bet,
+			decimal chance,
+			decimal multiplier,
+			bool isHigh,
+			decimal profit,
+			decimal balanceAfter,
+			int winMin,
+			int winMax)
+		{
+			Roll = roll;
+			IsWin = isWin;
+			Bet = bet;
+			Chance = chance;
+			Multiplier = multiplier;
+			IsHigh = isHigh;
+			Profit = profit;
+			BalanceAfter = balanceAfter;
+			WinMin = winMin;
+			WinMax = winMax;
+		}
 	}
 }
