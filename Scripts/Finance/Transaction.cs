e590868f8e5f0fc2769ddb7568059adb151491ee
zur11 <cibka11@gmail.com>
2026-02-17 23:59:13 -0500

    changed Transaction to do just the basics because the details will be handed by a new class that will be created: BetTransactionEvent

--- Scripts/Finance/Transaction.cs
+++ Scripts/Finance/Transaction.cs
@@ -1,28 +1,13 @@
-using System;
-
 namespace Scripts.Finance
-
 {
     public enum TransactionType
-	{
-		Deposit,
-		Withdrawal
-	}
+    {
+        Deposit,
+        Withdrawal
+    }
 
-	public class Transaction
-	{
-		public TransactionType Type { get; }
-		public decimal Amount { get; }
-		public DateTime Timestamp { get; }
-
-		public Transaction(TransactionType type, decimal amount)
-		{
-			if (amount <= 0m)
-				throw new ArgumentException("Transaction amount must be greater than zero.");
-
-			Type = type;
-			Amount = amount;
-			Timestamp = DateTime.UtcNow;
-		}
-	}
+    public sealed record Transaction(
+        TransactionType Type,
+        decimal Amount
+    );
 }
