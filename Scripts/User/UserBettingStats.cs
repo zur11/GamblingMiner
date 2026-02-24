83e2eb991fcb78cc976680b973a1bdbf3d1be68e
zur11 <cibka11@gmail.com>
2026-02-24 07:21:21 -0500

    improved UserBettingStats' functions structure, now everything is much more readable and clear

--- Scripts/User/UserBettingStats.cs
+++ Scripts/User/UserBettingStats.cs
@@ -1,50 +1,77 @@
+using System;
 using System.Collections.Generic;
 using Scripts.Finance;
 
 namespace Scripts.User
 {
-    public class UserBettingStats
-    {
-        private readonly List<UserBetRecord> _allBets = new();
+	public class UserBettingStats
+	{
+		private readonly List<UserBetRecord> _allBets = new();
 
-        public IReadOnlyList<UserBetRecord> AllBets => _allBets;
+		public IReadOnlyList<UserBetRecord> AllBets => _allBets.AsReadOnly();
 
-        public int BetsSinceLastDeposit { get; private set; }
-        public int TotalBets { get; private set; }
+		public int BetsSinceLastDeposit { get; private set; }
+		public int TotalBets { get; private set; }
 
-        public decimal AmountWageredSinceDeposit { get; private set; }
-        public decimal TotalAmountWagered { get; private set; }
+		public decimal AmountWageredSinceDeposit { get; private set; }
+		public decimal TotalAmountWagered { get; private set; }
 
-        public decimal ProfitSinceDeposit { get; private set; }
-        public decimal TotalProfit { get; private set; }
+		public decimal ProfitSinceDeposit { get; private set; }
+		public decimal TotalProfit { get; private set; }
 
-        public void RegisterBet(string gameId, BetTransactionEvent bet)
-        {
-            var record = new UserBetRecord(
-                gameId,
-                bet.Timestamp,
-                bet.BetAmount,
-                bet.Profit,
-                bet.IsWin
-            );
+		public void RegisterBet(string gameId, BetTransactionEvent bet)
+		{
+			if (bet == null)
+				throw new ArgumentNullException(nameof(bet));
 
-            _allBets.Add(record);
+			var record = CreateRecord(gameId, bet);
 
-            BetsSinceLastDeposit++;
-            TotalBets++;
+			AddRecord(record);
+			UpdateCounters(bet);
+			UpdateAmounts(bet);
+		}
 
-            AmountWageredSinceDeposit += bet.BetAmount;
-            TotalAmountWagered += bet.BetAmount;
+		public void RegisterDeposit()
+		{
+			ResetSessionCounters();
+		}
 
-            ProfitSinceDeposit += bet.Profit;
-            TotalProfit += bet.Profit;
-        }
+		private UserBetRecord CreateRecord(string gameId, BetTransactionEvent bet)
+		{
+			return new UserBetRecord(
+				gameId,
+				bet.Timestamp,
+				bet.BetAmount,
+				bet.Profit,
+				bet.IsWin
+			);
+		}
 
-        public void RegisterDeposit()
-        {
-            BetsSinceLastDeposit = 0;
-            AmountWageredSinceDeposit = 0m;
-            ProfitSinceDeposit = 0m;
-        }
-    }
+		private void AddRecord(UserBetRecord record)
+		{
+			_allBets.Add(record);
+		}
+
+		private void UpdateCounters(BetTransactionEvent bet)
+		{
+			BetsSinceLastDeposit++;
+			TotalBets++;
+		}
+
+		private void UpdateAmounts(BetTransactionEvent bet)
+		{
+			AmountWageredSinceDeposit += bet.BetAmount;
+			TotalAmountWagered += bet.BetAmount;
+
+			ProfitSinceDeposit += bet.Profit;
+			TotalProfit += bet.Profit;
+		}
+
+		private void ResetSessionCounters()
+		{
+			BetsSinceLastDeposit = 0;
+			AmountWageredSinceDeposit = 0m;
+			ProfitSinceDeposit = 0m;
+		}
+	}
 }
